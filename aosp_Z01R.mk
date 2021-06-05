90c66778100ea026b5b81891577d5c431c6d8953
Maynard Da Costa ( MAY{$}KER ) <dacostamaynard5@gmail.com>
2021-06-05 13:58:03 +0100

    Add files via upload

---
+++ aosp_Z01R.mk
@@ -0,0 +1,36 @@
+#
+# Copyright (C) 2018 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit from enchilada device
+$(call inherit-product, device/asus/Z01R/device.mk)
+
+# Inherit some common AOSP stuff.
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+
+# Boot animation
+TARGET_BOOT_ANIMATION_RES := 1080
+
+PRODUCT_NAME := aosp_Z01R
+PRODUCT_DEVICE := Z01R
+PRODUCT_MANUFACTURER := Asus
+PRODUCT_BRAND := Asus
+PRODUCT_MODEL := Zenfone 5z
+
+PRODUCT_GMS_CLIENTID_BASE := android-zenfone
